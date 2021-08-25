0a689053406af004b7abc9a933d6740d5d477548
Sivecano <Sivecano@gmail.com>
2021-08-25 10:05:11 +0200

    update

--- fetcher.py
+++ fetcher.py
@@ -2,5 +2,10 @@ import requests, json
 
 set = "8015-1"
 
-parts = requests.get("https://rebrickable.com/api/v3/lego/sets/" + set + "/parts/?key=cbd9de9e2b376ed4049d5be2be4c4457")
-print(json.fromstring(parts.text))
+with open("key.txt", "r") as f:
+    key = f.read().strip()
+
+parts = json.loads(requests.get("https://rebrickable.com/api/v3/lego/sets/" + set + "/parts/?key=" + key).text)
+
+for i in range(parts["count"]):
+    print(parts["results"][i]["quantity"],parts["results"][i]["part"]["part_num"])
