248835ee5cf7c1b5fc519a3b5c9827c6fc740168
Sivecano <Sivecano@gmail.com>
2021-08-25 10:34:37 +0200

    make it a function

--- fetcher.py
+++ fetcher.py
@@ -1,19 +1,21 @@
 import requests, json
 import pandas
 
-set = "8015-1"
 
-with open("key.txt", "r") as f:
-    key = f.read().strip()
+def get_partlist(set):
+    with open("key.txt", "r") as f:
+        key = f.read().strip()
 
-parts = json.loads(requests.get("https://rebrickable.com/api/v3/lego/sets/" + set + "/parts/?key=" + key).text)
+    parts = json.loads(requests.get("https://rebrickable.com/api/v3/lego/sets/" + set + "/parts/?key=" + key).text)
 
-out = []
+    out = []
 
-for i in range(parts["count"]):
-    out.append({"quantity" : parts["results"][i]["quantity"],
-                "part_num" : parts["results"][i]["part"]["part_num"],
-                "colours" : [b[0] for b in parts["results"][i]["color"]["external_ids"]["LDraw"]["ext_descrs"]]})
+    for i in range(parts["count"]):
+        out.append({"quantity" : parts["results"][i]["quantity"],
+                    "part_num" : parts["results"][i]["part"]["part_num"],
+                    "colours" : [b[0] for b in parts["results"][i]["color"]["external_ids"]["LDraw"]["ext_descrs"]]})
 
-print(pandas.DataFrame(out))
+    return pandas.DataFrame(out)
 
+if __name__ == "__main__":
+    print(get_partlist("8015-1"))
