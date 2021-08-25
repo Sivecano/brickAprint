db8db16494fa19fe25a373117b8d159e1f29ced7
Sivecano <Sivecano@gmail.com>
2021-08-25 10:46:06 +0200

    we support more than 100 items now

--- fetcher.py
+++ fetcher.py
@@ -4,18 +4,23 @@ import pandas
 
 def get_partlist(set):
     with open("key.txt", "r") as f:
-        key = f.read().strip()
+        url = "https://rebrickable.com/api/v3/lego/sets/" + set + "/parts/?key=" + f.read().strip()
 
-    parts = json.loads(requests.get("https://rebrickable.com/api/v3/lego/sets/" + set + "/parts/?key=" + key).text)
-
+    readcount = 0
     out = []
+    while True:
+        parts = json.loads(requests.get(url).text)
 
-    for i in range(parts["count"]):
-        out.append({"quantity" : parts["results"][i]["quantity"],
-                    "part_num" : parts["results"][i]["part"]["part_num"],
-                    "colours" : [b[0] for b in parts["results"][i]["color"]["external_ids"]["LDraw"]["ext_descrs"]]})
+        for i in range(min(parts["count"] - readcount * 100, 100)):
+            out.append({"quantity" : parts["results"][i]["quantity"],
+                        "part_num" : parts["results"][i]["part"]["part_num"],
+                        "colours" : [b[0] for b in parts["results"][i]["color"]["external_ids"]["LDraw"]["ext_descrs"]]})
+        if parts["next"] is None:
+            break
+        url = parts["next"]
+        readcount += 1
 
     return pandas.DataFrame(out)
 
 if __name__ == "__main__":
-    print(get_partlist("8015-1"))
+    print(get_partlist("21045-1"))
