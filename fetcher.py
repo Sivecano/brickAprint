84e514d9b272b9073c1960011241567d12d3ec39
Sivecano <Sivecano@gmail.com>
2021-08-25 10:28:47 +0200

    pandas

--- fetcher.py
+++ fetcher.py
@@ -1,4 +1,5 @@
 import requests, json
+import pandas
 
 set = "8015-1"
 
@@ -7,5 +8,12 @@ with open("key.txt", "r") as f:
 
 parts = json.loads(requests.get("https://rebrickable.com/api/v3/lego/sets/" + set + "/parts/?key=" + key).text)
 
+out = []
+
 for i in range(parts["count"]):
-    print(parts["results"][i]["quantity"],parts["results"][i]["part"]["part_num"])
+    out.append({"quantity" : parts["results"][i]["quantity"],
+                "part_num" : parts["results"][i]["part"]["part_num"],
+                "colours" : [b[0] for b in parts["results"][i]["color"]["external_ids"]["LDraw"]["ext_descrs"]]})
+
+print(pandas.DataFrame(out))
+
